bc7c46bece28e8124590f92199b35c7f0febb9c9
artem <crendelishe@rambler.ru>
2019-07-09 12:34:15 +0300

    V2.0

--- santander_luigi/constants.py
+++ santander_luigi/constants.py
@@ -2,11 +2,12 @@ from pathlib import Path
 
 HOME_PATH = Path.home()
 
-ELO_PATH = HOME_PATH / 'Santander-Customer-Transaction-Prediction'
-DATA_PATH = ELO_PATH / 'data'
+SANTANDER_PATH = HOME_PATH / 'Santander-Customer-Transaction-Prediction'
+DATA_PATH = SANTANDER_PATH / 'data'
 PIPE_PATH = DATA_PATH / 'pipeline'
 STG1_PATH = PIPE_PATH / 'stage_1'
 STG2_PATH = PIPE_PATH / 'stage_2'
 STG3_PATH = PIPE_PATH / 'stage_3'
+STG4_PATH = PIPE_PATH / 'stage_4'
 RESULT_PATH = PIPE_PATH / 'result'
 
--- santander_luigi/pipeline/luigi_pipeline.py
+++ santander_luigi/pipeline/luigi_pipeline.py
@@ -1,17 +1,16 @@
 import pandas as pd
 import numpy as np
-import pickle
 from pathlib import Path
 
 import luigi
 
-from santander_luigi.utils.data_utils import load_csv, load_pickle, save_pickle
+from santander_luigi.utils.data_utils import load_csv, load_pickle, save_pickle, reduce_memory_usage
 from santander_luigi.utils.pipeline_utils import posify
 from santander_luigi.features.make_features import make_FE_features
 from santander_luigi.train.train_models import train_catboost
 from santander_luigi.validation.make_validation import create_folds
 
-from santander_luigi.constants import STG1_PATH, STG2_PATH, STG3_PATH, RESULT_PATH, DATA_PATH
+from santander_luigi.constants import STG1_PATH, STG2_PATH, STG3_PATH, STG4_PATH, RESULT_PATH, DATA_PATH
 
 
 class DataFile(luigi.ExternalTask):
@@ -22,13 +21,32 @@ class DataFile(luigi.ExternalTask):
         return luigi.LocalTarget(posify(DATA_PATH / f'{self.data_type}.csv'))
 
 
+class ReduceMemory(luigi.Task):
+    data_type = luigi.Parameter(default='train')
+
+    def requires(self):
+        return DataFile(self.data_type)
+
+    def run(self):
+        data_req = self.input()
+
+        data = load_csv(data_req.path)
+        data = reduce_memory_usage(data)
+
+        with self.output().open('w') as csv_file:
+            data.to_csv(csv_file, index=False)
+
+    def output(self):
+        return luigi.LocalTarget(posify(STG1_PATH / f'{self.data_type}.csv'))
+
+
 class GetRealExamples(luigi.Task):
 
     def requires(self):
-        return (DataFile('train'), DataFile('test'))
+        return (ReduceMemory('train'), ReduceMemory('test'))
 
     def output(self):
-        return luigi.LocalTarget(posify(STG1_PATH / f'data.csv'))
+        return luigi.LocalTarget(posify(STG2_PATH / f'data.csv'))
 
     def run(self):
         samples_req = self.input()
@@ -57,10 +75,10 @@ class GetRealExamples(luigi.Task):
 class MakeFolds(luigi.Task):
 
     def requires(self):
-        return DataFile('train')
+        return ReduceMemory('train')
 
     def output(self):
-        return luigi.LocalTarget(posify(STG2_PATH / 'folds.pickle'))
+        return luigi.LocalTarget(posify(STG3_PATH / 'folds.pickle'))
 
     def run(self):
 
@@ -74,24 +92,28 @@ class TrainFoldsModel(luigi.Task):
 
     def requires(self):
         return {
-            'train': DataFile('train'),
+            'train': ReduceMemory('train'),
+            'test': ReduceMemory('test'),
             'data': GetRealExamples(),
             'folds': MakeFolds()
         }
 
     def output(self):
         return {
-            'models': luigi.LocalTarget(posify(STG3_PATH / f'catboost_models.pickle')),
-            'predictions': luigi.LocalTarget(posify(STG3_PATH / f'catboost_oof_preds.pickle')),
-            'features': luigi.LocalTarget(posify(STG3_PATH / f'feature_list.pickle'))
+            'models': luigi.LocalTarget(posify(STG4_PATH / f'catboost_models.pickle')),
+            'predictions': luigi.LocalTarget(posify(STG4_PATH / f'catboost_oof_preds.pickle')),
+            'features': luigi.LocalTarget(posify(STG4_PATH / f'feature_list.pickle')),
+            'test_df': luigi.LocalTarget(posify(STG4_PATH / f'test.csv'))
         }
 
     def run(self):
         train_req = self.input()['train']
+        test_req = self.input()['test']
         data_req = self.input()['data']
         folds_req = self.input()['folds']
 
         train: pd.DataFrame = load_csv(train_req.path)
+        test: pd.DataFrame = load_csv((test_req.path))
         data: pd.DataFrame = load_csv(data_req.path)
         folds: list = load_pickle(folds_req.path)
 
@@ -105,7 +127,10 @@ class TrainFoldsModel(luigi.Task):
             X_train, y_train = train.iloc[trn_idx][features], train.iloc[trn_idx]['target']
             X_valid, y_valid = train.iloc[val_idx][features], train.iloc[val_idx]['target']
 
-            X_train, X_valid = make_FE_features(X_train, data, features), make_FE_features(X_valid, data, features)
+            X_train, X_valid, test = make_FE_features(X_train, data, features),\
+                                     make_FE_features(X_valid, data, features),\
+                                     make_FE_features(test, data, features)
+
 
             clf, pred, score = train_catboost(X_train, y_train, X_valid, y_valid)
 
@@ -117,13 +142,15 @@ class TrainFoldsModel(luigi.Task):
         save_pickle(oof, Path(self.output()['predictions'].path))
         save_pickle(features, Path(self.output()['features'].path))
 
+        with self.output()['test_df'].open('w') as csv_file:
+            test.to_csv(csv_file, index = False)
+
 
 class GetSubmit(luigi.Task):
 
     def requires(self):
-        return {'test_df': DataFile('test'),
-                'trained_model': TrainFoldsModel()
-                }
+        return TrainFoldsModel()
+
 
     def output(self):
         return luigi.LocalTarget(posify(RESULT_PATH / f'submission.csv'))
@@ -131,8 +158,8 @@ class GetSubmit(luigi.Task):
     def run(self):
 
         test_req = self.input()['test_df']
-        models_req = self.input()['trained_model']['models']
-        features_req = self.input()['trained_model']['features']
+        models_req = self.input()['models']
+        features_req = self.input()['features']
 
         test = load_csv(test_req.path)
         models = load_pickle(models_req.path)
